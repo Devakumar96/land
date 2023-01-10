9d22a988b94085b517fc7398f238aabdd92b4f1f
Deva_kumar.N <83574761+Devakumar96@users.noreply.github.com>
2023-01-10 07:50:10 +0530

    link provided

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,33 +23,33 @@ const Footer = () => {
       <div className="col-6 col-md-2 mb-3">
         <h5>Info</h5>
         <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">About</a></li>
         </ul>
       </div>
 
       <div className="col-6 col-md-2 mb-3">
         <h5>Support</h5>
         <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">About</a></li>
         </ul>
       </div>
 
       <div className="col-6 col-md-2 mb-3">
         <h5>Connect</h5>
         <ul className="nav flex-column">
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Home</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Features</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">Pricing</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">FAQs</a></li>
-          <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-muted">About</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Home</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Features</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">Pricing</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">FAQs</a></li>
+          <li className="nav-item mb-2"><a href="https://saasmantra.com/" className="nav-link p-0 text-muted">About</a></li>
         </ul>
       </div>
 
